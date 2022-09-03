d38ff6aebc71d0bf467df115a6576dd8028d9c51
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-09-03 19:52:40 +0300

    profile picture component implemented

--- nexgen-input-task/src/components/personal-info/PersonalInfo.jsx
+++ nexgen-input-task/src/components/personal-info/PersonalInfo.jsx
@@ -14,18 +14,6 @@ import Bio from './bio/Bio';
 function PersonalInfo(){
     const {updateFormData} = useContext(FormContext);
 
-    const validateImageUpload = (e) => {
-        let maxSizeInKb = 3000;
-        let supportedTypes = ['image/jpeg', 'image/jpg']
-        if (e.target.files.length > 0) {
-            let {type, size} = e.target.files[0];
-            let sizeInKb = size/1000.00;
-            
-            if (!(sizeInKb <= maxSizeInKb) && !(supportedTypes.includes(type))) {
-                setErrors(prevState => {return {...prevState, profilePicture: 'Maximum image size is 3MB. Supported types are .jpeg and .jpg'}})
-            }
-        }
-    }
 
     return(
         <Wrapper id="personalInfo">
@@ -38,7 +26,7 @@ function PersonalInfo(){
             </Div>
             <Div>
                 <PhoneNumber />
-                {/* <ProfilePicture /> */}
+                <ProfilePicture />
                 {/* <Website /> */}
                 {/* <Bio />                 */}
             </Div>
--- nexgen-input-task/src/components/personal-info/profile-picture/ProfilePicture.jsx
+++ nexgen-input-task/src/components/personal-info/profile-picture/ProfilePicture.jsx
@@ -4,13 +4,49 @@ import {Label,ErrorMessage,FileUpload} from '../PersonalInfo.styles';
 
 
 function ProfilePicture(){
+    const {updateData} = useContext(FormContext);
+    const [input, setInput] = useState(null);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        if(input === ''){
+            setError('Password is required!');
+        }
+        else if(input === null){
+            return;
+        }
+        else if (input.length < 6 || input.length > 12) {
+            setError('Password must be between 6 and 12 characters.')
+        }
+        else{
+            updateData({password: input})
+            setError('');
+        }
+    }, [input]);
+
+    const validateImageUpload = (e) => {
+        let maxSizeInKb = 3000;
+        let supportedTypes = ['image/jpeg', 'image/jpg']
+        if (e.target.files.length > 0) {
+            let {type, size} = e.target.files[0];
+            let sizeInKb = size/1000.00;
+
+            if ((sizeInKb > maxSizeInKb) || (supportedTypes.find(x => x === type) === undefined)) {
+                setError('Maximum image size is 3MB. Supported types are .jpeg and .jpg');
+            }
+            else{
+                updateData({profilePicture: e.target.files[0]})
+                setError('');
+            }
+        }
+    }
 
     return(
         <>
             <Label>Profile picture:</Label>
             {/* validation for size */}
             <FileUpload type="file" name="profilePicture" onChange={validateImageUpload} />
-            <ErrorMessage>{errors.profilePicture}</ErrorMessage>
+            <ErrorMessage>{error}</ErrorMessage>
         </>
     )
 }
